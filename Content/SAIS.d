f5a60bf71cc559952c455a36fda56193255d1578
neocromicon <neocromicon@live.de>
2021-04-28 21:16:09 +0200

    Test

--- Content/SAIS.d
+++ Content/SAIS.d
@@ -375,4 +375,5 @@ func void Ninja_ShowAlwaysImportantStats_Main() {
 
     // Update the text and bar
     Ninja_ShowAlwaysImportantStats_Update(ypos, text, font, yFont, showBar, textHP, textMP);
+	test123;
 };
